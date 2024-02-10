23922287fbaa00607dddbc39556003fc5bd249ba
albert <albertcacv@gmail.com>
2024-02-10 19:15:21 +0100

    fix some pages

--- src/config.ts
+++ src/config.ts
@@ -31,20 +31,20 @@ export const SITE = {
 	// url: 'https://astro-ink.vercel.app',
 	githubUrl: 'https://github.com/t4rmop',
 	listDrafts: true,
-	image:
-		'https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png',
-	// YT video channel Id (used in media.astro)
-	ytChannelId: '',
-	// Optional, user/author settings (example)
-	// Author: name
-	author: '', // Example: Fred K. Schott
-	// Author: Twitter handler
-	authorTwitter: '', // Example: FredKSchott
-	// Author: Image external source
-	authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
-	// Author: Bio
-	authorBio:
-		'Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro',
+	// image:
+	// 	'https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png',
+	// // YT video channel Id (used in media.astro)
+	// ytChannelId: '',
+	// // Optional, user/author settings (example)
+	// // Author: name
+	// author: '', // Example: Fred K. Schott
+	// // Author: Twitter handler
+	// authorTwitter: '', // Example: FredKSchott
+	// // Author: Image external source
+	// authorImage: '', // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
+	// // Author: Bio
+	// authorBio:
+	// 	'Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro',
 };
 
 export const PAGE_SIZE = 8;
