0e85099457d28e8c2ce9db9958fd7de29775a7ba
albert <albertcacv@gmail.com>
2024-07-21 19:01:36 +0200

    update UI

--- src/projects.ts
+++ src/projects.ts
@@ -3,7 +3,9 @@ const projects = [
     title: 'rt63365e-bootloader',
     description: 'Doom running in ASUS router',
     tags: ['router', 'bootloader'],
-    href: 'https://github.com/taarmo/rt63365e-bootloader'
-  }
+    href: 'https://github.com/taarmo/rt63365e-bootloader',
+    urlImage: 'https://private-user-images.githubusercontent.com/98432158/266061063-849e7d5a-52ce-4f09-a1a8-0882c7e8de1e.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjE1NzkwNjYsIm5iZiI6MTcyMTU3ODc2NiwicGF0aCI6Ii85ODQzMjE1OC8yNjYwNjEwNjMtODQ5ZTdkNWEtNTJjZS00ZjA5LWExYTgtMDg4MmM3ZThkZTFlLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MjElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzIxVDE2MTkyNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWEzMTYxNDI3YjA2ODMwMzhjNjRhOTQzYmJiMzk1OGFlM2U3MWFjMjJjOTNiOTliMGIxZTM3ZDVlY2ZhYTBlOGYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.PZKqYN9gkhkV-DoxXI88OZOqtDIBqBoTpbNDZbkkTvg'
+  },
+
 ]
 export default projects
